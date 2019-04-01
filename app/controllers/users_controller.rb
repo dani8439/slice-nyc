4f45f85ff0040983e3cb5c25cfbc350ae4874c9f
dani8439 <dani8439@aol.com>
2019-04-01 13:50:05 -0400

    users controller, updated user route, user views basic mapping out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :password, presence: true
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
 end
