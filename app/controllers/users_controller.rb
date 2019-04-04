85a02bc4cf39764966588486717e894af7a705ca
dani8439 <dani8439@aol.com>
2019-04-04 14:32:10 -0400

    created user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
+  # before_action :require_login, only: [:show]
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :categories
   resources :ratings
   resources :restaurants
-  resources :users, only: [:new, :show]
+  resources :users, only: [:create, :show]
+
+  get '/signup' => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
