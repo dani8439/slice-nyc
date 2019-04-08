79cb17dc5573bf8b0ec81e0057bdfc9286d1c0b1
dani8439 <dani8439@aol.com>
2019-04-08 13:10:36 -0400

    getting login/logout working, added links to application layout and to views

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get '/login', :to => 'sessions#new'
   post '/login', :to => 'sessions#create'
   get '/auth/facebook/callback', :to => 'sessions#create'
-  get '/logout', :to => 'sessions#destroy'
+  delete '/logout', :to => 'sessions#destroy'
 
   root 'welcome#index'
 end
