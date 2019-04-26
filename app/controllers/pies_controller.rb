1149ce3f2b22470565fc4e9fdeb111ee05f4c3cd
dani8439 <dani8439@aol.com>
2019-04-26 15:02:18 -0400

    added methods to pies controller, amending pie views

--- app/controllers/pies_controller.rb
+++ app/controllers/pies_controller.rb
@@ -3,4 +3,18 @@ class PiesController < ApplicationController
   def index
     @pies = Pie.all
   end
+
+  def new
+    @pie = Pie.new
+  end
+
+  def show
+    @pie = Pie.find_by(id: params[:id])
+  end
+
+  private
+
+  def pie_params
+    params.require(:pie).permit(:name, :crust, :cheese, :shape)
+  end
 end
