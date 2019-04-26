bb3caabdfa72991859c5823f307025b6ac329a79
dani8439 <dani8439@aol.com>
2019-04-26 14:26:10 -0400

    added index routes and pages to pies and restaurants, added information to index pages for pies

--- app/controllers/pies_controller.rb
+++ app/controllers/pies_controller.rb
@@ -1,2 +1,6 @@
 class PiesController < ApplicationController
+
+  def index
+    @pies = Pie.all
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,5 @@
 class RestaurantsController < ApplicationController
+
+  def index
+  end
 end
