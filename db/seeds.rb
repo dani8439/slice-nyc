a9446f796152404db78aceb1e92633790e48e551
dani8439 <dani8439@aol.com>
2019-04-29 14:20:42 -0400

    modifying typos in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,19 @@ cat2 = Category.create(name: "Commercial Gas Oven", comments: "Most commercial p
 cat3 = Category.create(name: "Wood Fired", comments: "Wood-fire brick ovens use a small fire to heat a large solid oven. The oven holds the heat for a long time, so this type of cooking is very economical. Temperatures in a wood-fired oven can reach 800 °F (426 °C)."),
 
 
-pizza1 = Pie.create(name: "California/Gourmet Style", crust: "Thin", cheese: "Optional", shape: "Round"),
-pizza2 = Pie.create(name: "Chicago-Style/Deep Dish", crust: "Deep-dish", cheese: "Mozzarella", shape: "Round", pan: "Oiled Steel Pan"),
-pizza3 = Pie.create(name: "Chicago Thin Crust", crust: "Thin", cheese: "Mozzarella", shape: "Round"),
-pizza4 = Pie.create(name: "Detroit Style", crust: "Deep-Dish", cheese: "Mozzarella", shape: "Square", pan: "Oiled Blue Steel Pan or Cast Iron", additional_comments: "Carmelized Crust"),
-pizza5 = Pie.create(name: "Grandma Pie", crust: "Thin", cheese: "Mozzarella", shape: "Rectangle", pan: "Olive-Oil coated pan"),
-pizza6 = Pie.create(name: "Greek Style", crust: "Somewhere between crunchy and thicker Sicilian Cousin", cheese: "Mozzarella", shape: "Round", pan: "Oiled Shallow Pan"),
-pizza7 = Pie.create(name: "Italian Tomato Pie/New Jersey Style", crust: "Thin", cheese: "Grated Romano", shape: "Square", pan: "Steel Rectangular Pan", additional_comments: "Cheese is optional"),
-pizza8 = Pie.create(name: "Neopolitan", crust: "Thin", cheese: "Mozzarella", shape: "Round",),
-pizza9 = Pie.create(name: "NY Style", crust: "Thin", cheese: "Mozzarella", shape: "Round"),
-pizza10 = Pie.create(name: "Pizza Margherita", crust: "Thin", cheese: "Fresh Mozzarella", shape: "Round", additional_comments: "Pizza Margherita is a variant of Neopolitan Pizza."),
-pizza11 = Pie.create(name: "Sicilian Style", crust: "Thick", cheese: "Mozzarella", shape: "Rectangle"),
-pizza12 = Pie.create(name: "St. Louis Style", crust: "Cracker", cheese: "Provel", shape: "Round"),
-pizza13 = Pie.create(name: "White Clam Pie", crust: "Thin", shape: "Round", cheese: "Grated Romano", additional_comments: "Whole or Chopped clams, garlic, olive oil, and oregano"),
+pizza1 = Pie.create(name: "California/Gourmet Style", crust: "thin", cheese: "optional", shape: "round"),
+pizza2 = Pie.create(name: "Chicago-Style/Deep Dish", crust: "Deep-dish", cheese: "mozzarella", shape: "round", pan: "oiled steel pan"),
+pizza3 = Pie.create(name: "Chicago Thin Crust", crust: "thin", cheese: "mozzarella", shape: "round"),
+pizza4 = Pie.create(name: "Detroit Style", crust: "Deep-Dish", cheese: "mozzarella", shape: "square", pan: "oiled blue steel pan or cast iron", additional_comments: "A carmelized crust is a distinct feature of such a pizza."),
+pizza5 = Pie.create(name: "Grandma Pie", crust: "thin", cheese: "mozzarella", shape: "rectangular", pan: "olive-oil coated pan"),
+pizza6 = Pie.create(name: "Greek Style", crust: "somewhere between crunchy and thicker sicilian cousin", cheese: "mozzarella", shape: "round", pan: "oiled shallow pan"),
+pizza7 = Pie.create(name: "Italian Tomato Pie/New Jersey Style", crust: "thin", cheese: "grated romano", shape: "square", pan: "steel rectangular pan", additional_comments: "Cheese is optional."),
+pizza8 = Pie.create(name: "Neopolitan", crust: "thin", cheese: "mozzarella", shape: "round", additional_comments: "Has several different variations, such as Pizza Marinara and Pizza Margherita."),
+pizza9 = Pie.create(name: "NY Style", crust: "thin", cheese: "mozzarella", shape: "round"),
+pizza10 = Pie.create(name: "Pizza Margherita", crust: "thin", cheese: "fresh mozzarella", shape: "round", additional_comments: "Pizza Margherita is a variant of Neopolitan Pizza."),
+pizza11 = Pie.create(name: "Sicilian Style", crust: "thick", cheese: "mozzarella", shape: "rectangular"),
+pizza12 = Pie.create(name: "St. Louis Style", crust: "cracker", cheese: "provel", shape: "round", additional_comments: "Provel cheese is a combination of cheddar, Swiss and provolone cheeses."),
+pizza13 = Pie.create(name: "White Clam Pie", crust: "thin", shape: "round", cheese: "grated Romano", additional_comments: "Made with whole or chopped clams, garlic, olive oil, and oregano. Sometimes cheese is omitted."),
 
 
 
