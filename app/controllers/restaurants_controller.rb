915e4afe328d63ccf135ac2f59d1ea6f65459eb0
dani8439 <dani8439@aol.com>
2019-04-30 13:48:42 -0400

    adding information to restaurant pages, fixing styling, mapping out users adding own reviews to database.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,10 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find_by(id: params[:id])
   end
 end
