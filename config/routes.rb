1060942238bd30b87eebab0c29798f1e8a32fdc9
dani8439 <dani8439@aol.com>
2019-04-30 14:43:24 -0400

    more styling.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :pies
   resources :categories
   resources :restaurants
+  
+  # resources :categories do ??
+  #   resources :restaurants
+  # end
   resources :users, only: [:create, :show]
 
   get '/signup', :to => 'users#new'
