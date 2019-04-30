252cb7b20b14d453096f1e553c99e711a7b1049d
dani8439 <dani8439@aol.com>
2019-04-30 15:36:24 -0400

    added methods to restaurants controller for creating new restaurant, and added views.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name, :crust, :cheese)
+    params.require(:category).permit(:name, :crust, :cheese, :shape, :pan, :additional_comments)
   end
 
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,30 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find_by(id: params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :neighborhood, :borough, :sit_down_or_take_out, :oven, :multiple_locations, :category_id)
+  end
 end
