590140ef3fb1a5e07e43c578b2fd50f08b579245
dani8439 <dani8439@aol.com>
2019-05-03 15:17:29 -0400

    fixed error in collection select so restaurant can be saved, added display error message in new view for pie and restaurant, added create method to controller.

--- app/controllers/pies_controller.rb
+++ app/controllers/pies_controller.rb
@@ -12,7 +12,16 @@ class PiesController < ApplicationController
     @toppings = Topping.all
   end
 
+
+
   def create
+    @pie = Pie.new(pie_params)
+
+    if @pie.save
+      redirect_to @pie
+    else
+      render :new
+    end
   end
 
   def show
@@ -22,6 +31,6 @@ class PiesController < ApplicationController
   private
 
   def pie_params
-    params.require(:pie).permit(:name, :crust, cheese_ids: [], topping_ids: [])
+    params.require(:pie).permit(:name, :category_id, :sauce_id, :crust_id, :cheese_id, topping_ids: [])
   end
 end
