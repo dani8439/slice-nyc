a47de6c708361ab125e75280e58610a92952b412
dani8439 <dani8439@aol.com>
2019-05-17 15:20:02 -0400

    don't know what's going wrong with restuarant in schema.

--- db/migrate/20190402153341_create_restaurants.rb
+++ db/migrate/20190402153341_create_restaurants.rb
@@ -2,11 +2,11 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
       t.string :name
-      # t.string :neighborhood
-      # t.string :borough
-      # t.string :seating
-      # t.string :oven
-      # t.boolean :multiple_locations, :default => false
+      t.string :neighborhood
+      t.string :borough
+      t.string :seating
+      t.string :oven
+      t.boolean :multiple_locations, :default => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,11 @@ ActiveRecord::Schema.define(version: 2019_05_16_171411) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
+    t.string "neighborhood"
+    t.string "borough"
+    t.string "seating"
+    t.string "oven"
+    t.boolean "multiple_locations", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
