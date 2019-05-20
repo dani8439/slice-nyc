27e30e808b5844bbe48faae692269aa6b5fd452b
dani8439 <dani8439@aol.com>
2019-05-20 15:21:57 -0400

    still troubleshooting failure to allow restaurant record to save.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,20 +6,19 @@ class Restaurant < ApplicationRecord
   has_many :pie_restaurants
   has_many :pies, through: :pie_restaurants, :source => :pie
   has_many :restaurant_categories
-  has_many :categories, through: :restaurant_categories, :source => :category
+  has_many :categories, through: :restaurant_categories #, :source => :category
 
 
   validates :name, presence: true
   validates :neighborhood, presence: true
   validates :borough, presence: true
   validates :seating, presence: true
-  # validates :category_ids, presence: true
 
   accepts_nested_attributes_for :pies
   accepts_nested_attributes_for :categories
 
 
-  #Rack::QueryParser::ParameterTypeError (expected Array (got String) for param `name'): ?? This why restaurant isn't being saved? What's happening here? 
+  #Rack::QueryParser::ParameterTypeError (expected Array (got String) for param `name'): ?? This why restaurant isn't being saved? What's happening here?
 
 
 
