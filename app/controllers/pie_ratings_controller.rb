3c36262f098d0dde0b500c2ba5a85b8cfa8e483c
dani8439 <dani8439@aol.com>
2019-05-28 14:48:13 -0400

    fixed form, and moved into partial for pie_rating.

--- app/controllers/pie_ratings_controller.rb
+++ app/controllers/pie_ratings_controller.rb
@@ -5,15 +5,14 @@ class PieRatingsController < ApplicationController
   end
 
   def new
-    @user = current_user
     @restaurant = Restaurant.all
     @pie_rating = PieRating.new
-    #@pie_rating = @user.pie_ratings.build
+    # @pie_rating = @user.pie_ratings.build
   end
 
   def create
-    #@pie_rating = @user.pie_ratings.build(pie_rating_params)
-    @pie_rating = PieRating.create(pie_rating_params)
+    @user = current_user
+    @pie_rating = @user.pie_ratings.build(pie_rating_params)
 
     if @pie_rating.save
       redirect_to pie_rating_path(@pie_rating)
@@ -22,12 +21,15 @@ class PieRatingsController < ApplicationController
     end
   end
 
+  def edit
+  end 
+
   def show
   end
 
   private
   def pie_rating_params
-    params.require(:pie_rating).permit(:pie_id, :user_id, :taste_score, :comments)
+    params.require(:pie_rating).permit(:pie_id, :taste_score, :comments)
   end
 
 end
