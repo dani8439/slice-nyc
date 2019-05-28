72b0358ff0aef55a499434fcdde9e37b1f265310
dani8439 <dani8439@aol.com>
2019-05-28 11:02:03 -0400

    adding retaurant_id to pie_ratings does not work, causes too many errors, a user cannot update a restaurant itself because of it.

--- app/models/pie_rating.rb
+++ app/models/pie_rating.rb
@@ -1,11 +1,11 @@
 class PieRating < ApplicationRecord
   belongs_to :pie
   belongs_to :user
-  belongs_to :restaurant 
+  # belongs_to :restaurant
 
 
 
-  validates :taste_score, presence: true
+  validates :taste_score, presence: true #will cause error when trying to update restaurant itself.
   # validates :pie_id, presence: true
   # validates :user_id, presence: true
 
