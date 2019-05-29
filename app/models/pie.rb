6789c292ab6b4cba877782a7ed0eff2454f036a5
dani8439 <dani8439@aol.com>
2019-05-29 16:08:31 -0400

    fixed seeds.

--- app/models/pie.rb
+++ app/models/pie.rb
@@ -1,13 +1,14 @@
 class Pie < ApplicationRecord
   belongs_to :crust
   belongs_to :sauce
-  belongs_to :category 
+  belongs_to :category
 
   has_many :pie_cheeses
   has_many :cheeses, through: :pie_cheeses
   has_many :pie_toppings
   has_many :toppings, through: :pie_toppings
   has_many :pie_ratings
+  has_many :restaurant_ratings, through: :pie_ratings 
   # has_many :users, through: :pie_ratings
   # has_many :restaurants, through: :pie_ratings
   has_many :pie_restaurants
