2dd2a3766b77d6e58b1b30c50a0af63c748a7971
dani8439 <dani8439@aol.com>
2019-06-18 15:51:27 -0400

    reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 
-  resources :restaurant_categories #refactor to not show these routes as they are not being used?? 
-  resources :pie_restaurants
+  resources :restaurant_categories #refactor to not show these routes as they are not being used??
+  resources :pie_restaurants 
   resources :pie_toppings
   resources :pie_cheeses
   resources :categories
