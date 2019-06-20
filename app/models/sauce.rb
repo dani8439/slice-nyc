d577bc273fcfd818d4aacf4f35c35e8f7d32072b
dani8439 <dani8439@aol.com>
2019-06-20 15:07:21 -0400

    added scope method in Sauce model.

--- app/models/sauce.rb
+++ app/models/sauce.rb
@@ -1,4 +1,10 @@
 class Sauce < ApplicationRecord
   has_many :pies, :class_name => 'Pie'
 
+
+  def find_pies
+    Pie.joins(:sauce).where(pies: { sauce_id: self.id })
+    # SELECT "pies".* FROM 'pies' INNER JOIN "sauces" ON "sauce"."id" = "pies"."sauce_id" WHERE "pies"."sauce_id" = ? LIMIT ?
+  end
+
 end
