a3fc6712cc3ff0b6814d761bdc54530adcce106e
dani8439 <dani8439@aol.com>
2019-06-24 15:16:59 -0400

    .find_pies no longer needed, perhaps a blip that @crust.pies wasn't callable last week, but now is?

--- app/models/crust.rb
+++ app/models/crust.rb
@@ -1,8 +1,8 @@
 class Crust < ApplicationRecord
   has_many :pies, :class_name => 'Pie'
 
-  def find_pies
-    Pie.joins(:crust).where(pies: { crust_id: self.id })
-    # SELECT "pies".* FROM "pies" INNER JOIN "crusts" ON "crusts"."id" = "pies"."crust_id" WHERE "pies"."crust_id" = ? LIMIT ?
-  end
+  # def find_pies
+  #   Pie.joins(:crust).where(pies: { crust_id: self.id })
+  #   # SELECT "pies".* FROM "pies" INNER JOIN "crusts" ON "crusts"."id" = "pies"."crust_id" WHERE "pies"."crust_id" = ? LIMIT ?
+  # end
 end
